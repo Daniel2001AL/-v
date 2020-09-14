0d7e585d3267f2ff595034155101a9df3f325ddf
Daniel2001AL <193290@ids.upchiapas.edu.mx>
2020-09-14 12:55:33 -0500

    upload project with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingPageComponentComponent } from './landing-page-component/landing-page-component.component'
+
+const routes: Routes = [
+  {path : '', redirectTo: 'landing-page', pathMatch : 'full'},
+  {path : 'landing-page', component : LandingPageComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
